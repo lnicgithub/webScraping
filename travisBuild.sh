b0321f25cc360260107ada31b38e9eff73fe7d6e
lnic <lnicgithub@github>
2020-03-29 14:28:35 +0100

    moving to buildx

--- travisBuild.sh
+++ travisBuild.sh
@@ -1,26 +1,29 @@
 # Login into docker
 docker login --username $DOCKER_USER --password $DOCKER_PASSWORD
+export DOCKER_CLI_EXPERIMENTAL=enabled
+
+docker buildx build --platform linux/arm/v7,linux/amd64 --push -t lnicdockerhub/app:multi_arch_latest .
 
 # Build for amd64 and push
-buildctl build --frontend dockerfile.v0 \
-            --local dockerfile=. \
-            --local context=. \
-            --exporter image \
-            --exporter-opt name=lnicdockerhub/app:test-build-amd64 \
-            --exporter-opt push=true \
-            --frontend-opt platform=linux/amd64 \
-            --frontend-opt filename=./Dockerfile
+# buildctl build --frontend dockerfile.v0 \
+#             --local dockerfile=. \
+#             --local context=. \
+#             --exporter image \
+#             --exporter-opt name=lnicdockerhub/app:test-build-amd64 \
+#             --exporter-opt push=true \
+#             --frontend-opt platform=linux/amd64 \
+#             --frontend-opt filename=./Dockerfile
 
 
-# Build for armhf and push
-buildctl build --frontend dockerfile.v0 \
-            --local dockerfile=. \
-            --local context=. \
-            --exporter image \
-            --exporter-opt name=lnicdockerhub/app:test-build-armhf \
-            --exporter-opt push=true \
-            --frontend-opt platform=linux/arm64 \
-            --frontend-opt filename=./Dockerfile
+# # Build for armhf and push
+# buildctl build --frontend dockerfile.v0 \
+#             --local dockerfile=. \
+#             --local context=. \
+#             --exporter image \
+#             --exporter-opt name=lnicdockerhub/app:test-build-armhf \
+#             --exporter-opt push=true \
+#             --frontend-opt platform=linux/arm64 \
+#             --frontend-opt filename=./Dockerfile
 
 
-export DOCKER_CLI_EXPERIMENTAL=enabled
+
