526de912a8e18658009aaa788f7919b44e4cfd0f
lnic <lnicgithub@github>
2020-03-30 08:32:17 +0100

    changed buildx structure

--- travisBuild.sh
+++ travisBuild.sh
@@ -1,20 +1,53 @@
-export DOCKER_CLI_EXPERIMENTAL=enabled
+export DOCKER_PLATFORMS='linux/amd64'
+DOCKER_PLATFORMS+=' linux/arm64'
+DOCKER_PLATFORMS+=' linux/arm/v6'
 BUILDX_VER=v0.3.0
 IMAGE_NAME=lnicdockerhub/app
 VERSION=latest
 CI_NAME=travis
 
-mkdir -vp ~/.docker/cli-plugins/ ~/dockercache
-curl --silent -L "https://github.com/docker/buildx/releases/download/${BUILDX_VER}/buildx-${BUILDX_VER}.linux-amd64" > ~/.docker/cli-plugins/docker-buildx
-chmod a+x ~/.docker/cli-plugins/docker-buildx
+sudo rm -rf /var/lib/apt/lists/*
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) edge"
+sudo apt-get update
+sudo apt-get -y -o Dpkg::Options::="--force-confnew" install docker-ce
 
-docker run --rm --privileged multiarch/qemu-user-static:register --reset -p
-docker buildx create --use
+local -r config='/etc/docker/daemon.json'
+  if [[ -e "$config" ]]; then
+    sudo sed -i -e 's/{/{ "experimental": true, /' "$config"
+  else
+    echo '{ "experimental": true }' | sudo tee "$config"
+  fi
+sudo systemctl restart docker
+
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+export DOCKER_CLI_EXPERIMENTAL=enabled
+docker buildx create --name mybuilder
+docker buildx use mybuilder
+# Start up buildx and verify that all is OK.
 docker buildx inspect --bootstrap
-docker buildx build --push \
-		--build-arg CI_NAME=${CI_NAME} \
-		--platform linux/arm/v7,linux/arm64/v8,linux/386,linux/amd64 \
-		-t ${IMAGE_NAME}:${VERSION}-${CI_NAME} .
+docker login -u "$DOCKER_USER" -p "$DOCKER_PASSWORD"
+
+docker buildx build \
+    --platform "${DOCKER_PLATFORMS// /,}" \
+    --push \
+    --progress plain \
+    -f Dockerfile \
+    -t ${IMAGE_NAME}:${VERSION}-${CI_NAME} \
+    .
+
+
+# mkdir -vp ~/.docker/cli-plugins/ ~/dockercache
+# curl --silent -L "https://github.com/docker/buildx/releases/download/${BUILDX_VER}/buildx-${BUILDX_VER}.linux-amd64" > ~/.docker/cli-plugins/docker-buildx
+# chmod a+x ~/.docker/cli-plugins/docker-buildx
+
+# docker run --rm --privileged multiarch/qemu-user-static:register --reset -p yes
+# docker buildx create --use
+# docker buildx inspect --bootstrap
+# docker buildx build --push \
+# 		--build-arg CI_NAME=${CI_NAME} \
+# 		--platform linux/arm/v7,linux/arm64/v8,linux/386,linux/amd64 \
+# 		-t ${IMAGE_NAME}:${VERSION}-${CI_NAME} .
 
 
 # Build for amd64 and push
