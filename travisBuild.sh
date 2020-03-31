86c97dc9ee2b6260264e551b21d06ff354fff1a6
lnic <lnicgithub@github>
2020-03-31 22:15:01 +0100

    initial heroku config

--- travisBuild.sh
+++ travisBuild.sh
@@ -53,7 +53,7 @@ docker buildx build \
 
 # Heroku Deploy
 # install heroku CLI
-wget -qO- https://toolbelt.heroku.com/install.sh | sh
+curl https://cli-assets.heroku.com/install.sh | sh
 docker login -u "$HEROKU_USER" -p "$HEROKU_PASSWORD" registry.heroku.com
 echo ${IMAGE_NAME}
 echo ${VERSION}
@@ -61,4 +61,4 @@ echo ${CI_NAME}
 echo $HEROKU_APP_NAME
 docker tag ${IMAGE_NAME}:${VERSION}-${CI_NAME} registry.heroku.com/$HEROKU_APP_NAME/web
 docker push registry.heroku.com/$HEROKU_APP_NAME/web
-heroku container: release web --app $HEROKU_APP_NAME
+heroku container:release web --app $HEROKU_APP_NAME
