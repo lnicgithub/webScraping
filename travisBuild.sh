2aa6868776db3858d23a3e32a06e395dc24ccc62
lnic <lnicgithub@github>
2020-04-01 14:19:25 +0100

    updating variables

--- travisBuild.sh
+++ travisBuild.sh
@@ -3,7 +3,7 @@
 # context form https://nexus.eddiesinentropy.net/2020/01/12/Building-Multi-architecture-Docker-Images-With-Buildx/
 
 # set some parameters for Docker. Platforms must be space delimited.
-IMAGE_NAME=lnicdockerhub/app
+IMAGE_NAME='lnicdockerhub/app'
 VERSION=latest
 CI_NAME=travis
 export DOCKER_PLATFORMS='linux/amd64'
@@ -59,6 +59,6 @@ echo ${IMAGE_NAME}
 echo ${VERSION}
 echo ${CI_NAME}
 echo $HEROKU_APP_NAME
-docker tag "$IMAGE_NAME:$VERSION-$CI_NAME" registry.heroku.com/$HEROKU_APP_NAME/web
+docker tag ${IMAGE_NAME}:${VERSION}-${CI_NAME} registry.heroku.com/$HEROKU_APP_NAME/web
 docker push registry.heroku.com/$HEROKU_APP_NAME/web
 /usr/local/bin/heroku container:release web --app $HEROKU_APP_NAME
