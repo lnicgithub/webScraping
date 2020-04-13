294025dbceeb648a387d1d806f0b54c9abba3dba
lnic <lnicgithub@github>
2020-04-13 16:09:42 +0100

    tidy up

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,17 +1,9 @@
 import express from "express";
-import dataStore from "../models/dbModel";
 
 const router = express.Router();
 
 /* GET home page. */
 router.get("/", (req, res) => {
-  dataStore.insert({ testfdf: "testEttntry" }, function(err, newRec) {
-    if (err) {
-      return console.log(err);
-    }
-    return newRec;
-  });
-
   res.send("index site");
 });
 
